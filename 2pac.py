cfeab58676bb30c107dd8a64b5322600a9e00a2b
LegendxSidhu <131896738+LegendxSidhu@users.noreply.github.com>
2023-04-26 21:55:09 +0500

    Update 2pac.py

--- 2pac.py
+++ 2pac.py
@@ -1549,7 +1549,7 @@ def RDX6(ids,passlist):
 			"email":uid,
 			"pass":ps,
 			"login":"Log In"}
-			header_freefb = {'authority':'mbasic.facebook.com;
+			header_freefb = {'authority''mbasic.facebook.com;
 	'method': 'path;
         'scheme': 'https;
                         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
